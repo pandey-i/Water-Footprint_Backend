1448113c931890dbb2a5e49777e7c2d4ee1012a6
Aman1184 <amanpandey94158@gmail.com>
2025-05-26 02:23:20 +0530

    new2

--- myenv/app.py
+++ myenv/app.py
@@ -10,7 +10,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-                 "https://water-footprint-backend.vercel.app/"],  # Change this to your frontend URL in production
+                 "https://water-footprint.netlify.app/"],  # Change this to your frontend URL in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
